f9fc8718406db62186bd21d78a0c6c30fec418ab
Ahmed Nabil <Ahmed.Nabil@apoteket.se>
2022-09-05 11:07:45 +0200

    Add image_uri

--- dbContext.js
+++ dbContext.js
@@ -1,54 +1,56 @@
-const NodeCache = require( "node-cache" );
-var  config = require('./dbConfig');
-const  sql = require('mssql');
+const NodeCache = require("node-cache");
+var config = require("./dbConfig");
+const sql = require("mssql");
 
-const myCache = new NodeCache({ stdTTL: 60*60*2});
+const myCache = new NodeCache({ stdTTL: 60 * 60 * 2 });
 
-async  function  getOrders() {
-    try {
-      let  pool = await  sql.connect(config);
-      let  products = await  pool.request().query("select [Id],muscles,Name,[difficulty],BodyParts,[Equipment],[excercise-gif],[ex-image],[ex-steps]   FROM [ExercisesDB].[dbo].[exercises]");
-      return  products.recordsets;
-    }
-    catch (error) {
-      console.log(error);
-    }
+async function getOrders() {
+  try {
+    let pool = await sql.connect(config);
+    let products = await pool
+      .request()
+      .query(
+        "select [Id],muscles,Name,[difficulty],BodyParts,[Equipment],[excercise-gif],[ex-image],[ex-steps]   FROM [ExercisesDB].[dbo].[exercises]"
+      );
+    return products.recordsets;
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  async function getMuscles()
-  {
-    try {
-      if(myCache.has( 'muscles' ))
-      {
-        return myCache.get( 'muscles');
-      }
-      let  pool = await  sql.connect(config);
-      let  muscles = await  pool.request().query("SELECT [Id],[Name] ,[name_en] FROM [dbo].[muscles]");
-
-      myCache.set( 'muscles',muscles.recordsets);
-
-      return  muscles.recordsets;
+async function getMuscles() {
+  try {
+    if (myCache.has("muscles")) {
+      return myCache.get("muscles");
     }
-    catch (error) {
-      console.log(error);
-    }
+    let pool = await sql.connect(config);
+    let muscles = await pool
+      .request()
+      .query("SELECT [Id],[Name] ,[name_en], [image_uri] FROM [dbo].[muscles]");
+
+    myCache.set("muscles", muscles.recordsets);
+
+    return muscles.recordsets;
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  async  function  getOrder(productId) {
-    try {
-      let  pool = await  sql.connect(config);
-      let  product = await  pool.request()
-      .input('input_parameter', sql.Int, productId)
+async function getOrder(productId) {
+  try {
+    let pool = await sql.connect(config);
+    let product = await pool
+      .request()
+      .input("input_parameter", sql.Int, productId)
       .query("SELECT * from Orders where Id = @input_parameter");
-      return  product.recordsets;
-    }
-    catch (error) {
-      console.log(error);
-    }
+    return product.recordsets;
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  module.exports = {
-    getOrders:  getOrders,
-    getOrder:  getOrder,
-    getMuscles : getMuscles
-  }
+module.exports = {
+  getOrders: getOrders,
+  getOrder: getOrder,
+  getMuscles: getMuscles,
+};
